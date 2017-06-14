960e2cdc3cd55c34b657acbf086a08d0e8271dbb
garthdb <garthdb@gmail.com>
2017-06-14 10:43:59 -0700

    fixed routes for us2017

--- src/routes.jsx
+++ src/routes.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router'
 
 import Root from './components/root.jsx'
 import Index from './components/index.jsx'
-import us2017 from './components/us2017.jsx'
+import Us2017 from './components/us2017.jsx'
 import Eu2017 from './components/eu2017.jsx'
 import SpeakerPage from './components/speaker-page-us.jsx'
 import SpeakerPageEU from './components/speaker-page-eu.jsx'
@@ -14,7 +14,7 @@ export default (
     <IndexRoute component={Index} />
     <Route path="/eu2017/" component={Eu2017} onLeave={() => {window.fromHome = true}} />
     <Route path="/eu2017/speaker/:slug" component={SpeakerPageEU} onLeave={() => {window.fromHome = true}} />
+    <Route path="/us2017/" component={Us2017} onLeave={() => {window.fromHome = true}} />
+    <Route path="/us2017/speaker/:slug" component={SpeakerPage} />
   </Route>
 )
-// <Route path="/us2017/" component={us2017} onLeave={() => {window.fromHome = true}} />
-// <Route path="/us2017/speaker/:slug" component={SpeakerPage} />
